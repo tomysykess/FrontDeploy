7a1e74583ea7394125c15c9d052ea5c8fc9991f8
tomas.avila <tomysykess@gmail.com>
2024-06-13 13:53:42 -0300

    token

--- liquors/src/components/authStore/AuthStore.tsx
+++ liquors/src/components/authStore/AuthStore.tsx
@@ -6,25 +6,34 @@ import { Provider } from "react-redux";
 interface AuthProps {
   children: React.ReactNode;
 }
+interface UserToken {
+  email: string;
+  firebaseUid: string;
+  id: string;
+  name: string;
+  provider: any;
+  role: number;
+  token: string;
+}
 
 const AuthStore: React.FC<AuthProps> = ({ children }) => {
-  const [token, setToken] = useState<string | null>(null); // Inicializa el estado con el tipo adecuado
+  const [token, setToken]: any = useState(); // Inicializa el estado con el tipo adecuado
 
   useEffect(() => {
     const fetchUserData = async () => {
       const userDataLogin = localStorage.getItem("userDataLogin");
 
       if (userDataLogin) {
-        const userData = JSON.parse(userDataLogin);
+        const userData: UserToken = JSON.parse(userDataLogin);
         const userId = userData.id;
         const userToken = userData.token;
-        setToken(userToken);
-
+        setToken(userToken!);
+        console.log("este es el token que tiene en el storages", token);
         try {
           const response = await axios.get(
             `https://liquors-project.onrender.com/users/${userId}`
           );
-          var newUserData = response.data;
+          var newUserData: UserToken = response.data;
 
           if (newUserData.role !== userData.role) {
             console.log("Token cambiado");
