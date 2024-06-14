4f042041bb8e4b8dd9fdd26a7f52ed3e9c47e1ce
tomas.avila <tomysykess@gmail.com>
2024-06-14 19:42:41 -0300

    footer error hidratacion

--- liquors/src/components/footer/footer.tsx
+++ liquors/src/components/footer/footer.tsx
@@ -2,10 +2,10 @@ import Link from "next/link";
 
 export const Footer = () => {
   return (
-    <div className="flex justify-around items-center p-3 mx-large my-9">
-      <div className="">
+    <div className="flex justify-around items-center p-3 mx-large my-9 ">
+      <div>
         <Link href="/">
-          <p className="font-plus-jakarta-sans text-3xl font-extrabold text-wine ">
+          <p className="font-plus-jakarta-sans text-3xl font-extrabold text-wine">
             Liquors
           </p>
         </Link>
@@ -14,31 +14,33 @@ export const Footer = () => {
         <ul className="flex space-x-6">
           <li className="font-plus-jakarta-sans">
             Contact Us
-            <div className="flex flex-col">
+            <ul className="flex flex-col">
               <li className="font-plus-jakarta-sans">
-                Street Name 123, City, Country{" "}
+                Street Name 123, City, Country
               </li>
-              <li className="font-plus-jakarta-sans">Phone: (123) 456-7890 </li>
-              <li className="font-plus-jakarta-sans">info@example.com </li>
-            </div>
+              <li className="font-plus-jakarta-sans">Phone: (123) 456-7890</li>
+              <li className="font-plus-jakarta-sans">info@example.com</li>
+            </ul>
           </li>
-          <div className="flex flex-col">
-            <li>
-              <Link className="buttonSecondary" href="/recommendation">
-                Pick of the Month
-              </Link>
-            </li>
-            <li>
-              <Link className="buttonSecondary" href="/newsletter">
-                Newsletter
-              </Link>
-            </li>
-            <li>
-              <Link className="buttonSecondary" href="/aboutUs">
-                About Us
-              </Link>
-            </li>
-          </div>
+          <li className="flex flex-col">
+            <ul className="flex flex-col">
+              <li>
+                <Link className="buttonSecondary" href="/recommendation">
+                  Pick of the Month
+                </Link>
+              </li>
+              <li>
+                <Link className="buttonSecondary" href="/newsletter">
+                  Newsletter
+                </Link>
+              </li>
+              <li>
+                <Link className="buttonSecondary" href="/aboutUs">
+                  About Us
+                </Link>
+              </li>
+            </ul>
+          </li>
         </ul>
       </div>
     </div>
