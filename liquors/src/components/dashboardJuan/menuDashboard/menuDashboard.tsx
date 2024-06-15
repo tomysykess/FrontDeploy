7b91f685cb4f6e066a7ccfb899ac86c706b03ce4
Juanch <Juanpablo_delrosso@outlook.com.ar>
2024-06-15 04:28:47 -0300

    -agregue icono candado a rutas de usuario premiun para la vista de un usuario normal, si clikea muestra una sweet alert notificando que debe ser premiun para esa  pagina.

--- liquors/src/components/dashboardJuan/menuDashboard/menuDashboard.tsx
+++ liquors/src/components/dashboardJuan/menuDashboard/menuDashboard.tsx
@@ -2,8 +2,15 @@ import React,{useState, useEffect} from "react";
 import Link from "next/link";
 import { UserDataLogin } from "@/interfaces/interfaz";
 import { usePathname } from "next/navigation";
+import LockIcon from '@mui/icons-material/Lock';
+import Swal from 'sweetalert2';
+import ReviewsIcon from '@mui/icons-material/Reviews';
+import FavoriteTwoToneIcon from '@mui/icons-material/FavoriteTwoTone';
+import SettingsIcon from '@mui/icons-material/Settings';
+import CardGiftcardIcon from '@mui/icons-material/CardGiftcard';
+
 export const MenuDashboard: React.FC = (): React.ReactNode => {
-    
+
     const pathname = usePathname();
 
     const [ dataUser, setDataUser ] = useState({
@@ -19,6 +26,17 @@ export const MenuDashboard: React.FC = (): React.ReactNode => {
         setDataUser(JSON.parse(storeData!));
         }
     }, [pathname])
+
+    
+    //HANDLER PARA RUTAS ROL 1
+    const routeHandlerRole1 = () => {
+         Swal.fire({
+            icon: 'info',
+            title: 'Acceso Denegado',
+            text: 'Debes ser usuario premium para acceder a esta página',
+            confirmButtonText: 'Aceptar'
+        });
+    }
     
     return (
         <>
@@ -35,9 +53,13 @@ export const MenuDashboard: React.FC = (): React.ReactNode => {
                             <nav className="flex-1 pt-6">
                                 <ul className="space-y-2">
 
-                                    <Link href="/profile" className="block py-2 px-6 text-white hover:text-wine hover:bg-greyMLfilter transition duration-300">Informacion de la cuenta</Link>
+                                    <Link href="/profile" className="block py-2 px-6 text-white hover:text-wine hover:bg-greyMLfilter transition duration-300"><SettingsIcon/>Informacion de la cuenta</Link>
                                     {dataUser.role == "1" && (
-                                        <Link href="/profile/dashboardUser/favoritos" className="block py-2 px-6 text-white hover:text-wine hover:bg-greyMLfilter transition duration-300">Favoritos</Link>  
+                                        <>
+                                        <span  onClick={routeHandlerRole1} className="block cursor-pointer py-2 px-6 text-white hover:text-wine hover:bg-greyMLfilter transition duration-300"><LockIcon/>Historial de reviews</span >
+                                        <Link href="/profile/dashboardUser/favoritos" className="block py-2 px-6 text-white hover:text-wine hover:bg-greyMLfilter transition duration-300"><FavoriteTwoToneIcon/>Favoritos</Link> 
+                                        <span   onClick={routeHandlerRole1} className="block cursor-pointer py-2 px-6 text-white hover:text-wine hover:bg-greyMLfilter transition duration-300"><LockIcon/>Cajas del mes</span >
+                                        </>  
                                     )}
                                     {/*SUPER ADMIN ES ROL 2 */}
                                     {dataUser.role == "2" && (
@@ -51,9 +73,9 @@ export const MenuDashboard: React.FC = (): React.ReactNode => {
                                     )}
                                     {dataUser.role == "4" && (
                                         <>
-                                           <Link href="/profile/dashboardUser/historialReviews" className="block py-2 px-6 text-white hover:text-wine hover:bg-greyMLfilter transition duration-300">Historial de reviews</Link>  
-                                           <Link href="/profile/dashboardUser/favoritos" className="block py-2 px-6 text-white hover:text-wine hover:bg-greyMLfilter transition duration-300">Favoritos</Link>  
-                                           <Link href="/profile/dashboardUser/cajaMes" className="block py-2 px-6 text-white hover:text-wine hover:bg-greyMLfilter transition duration-300">Cajas del mes</Link>  
+                                           <Link href="/profile/dashboardUser/historialReviews" className="block py-2 px-6 text-white hover:text-wine hover:bg-greyMLfilter transition duration-300">< ReviewsIcon/>Historial de reviews</Link>  
+                                           <Link href="/profile/dashboardUser/favoritos" className="block py-2 px-6 text-white hover:text-wine hover:bg-greyMLfilter transition duration-300"><FavoriteTwoToneIcon/>Favoritos</Link>  
+                                           <Link href="/profile/dashboardUser/cajaMes" className="block py-2 px-6 text-white hover:text-wine hover:bg-greyMLfilter transition duration-300"><CardGiftcardIcon/>Cajas del mes</Link>  
                                         </>
                                     )}
                                 </ul>
