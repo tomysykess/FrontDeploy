37630e00c088361471ca9fe6cad0f29787f9e5ec
Juanch <Juanpablo_delrosso@outlook.com.ar>
2024-06-18 02:57:15 -0300

    -arreglos de problemas de estilos surgidos al hacer git pull orgiin main.

--- liquors/src/app/profile/dashboardUser/cajaMes/page.tsx
+++ liquors/src/app/profile/dashboardUser/cajaMes/page.tsx
@@ -1,11 +1,11 @@
 'use client'
-import { MapUserFavoriteProducts } from "@/components/dashboardJuan/favoriteProducts/mapFavoriteProducts";
 import { MenuDashboard } from "@/components/dashboardJuan/menuDashboard/menuDashboard";
 import React,{ useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import LabelImportantIcon from '@mui/icons-material/LabelImportant';
+import SubscriptionBox from "@/components/cajaDelMes/caja";
 
-const FavoriteProduct: React.FC = (): React.ReactNode => {
+const CajaMes: React.FC = (): React.ReactNode => {
 
     const [token, setToken] = useState<string | null>(null);
     const [role, setRole] = useState<any>({rol: ""})
@@ -35,16 +35,16 @@ const FavoriteProduct: React.FC = (): React.ReactNode => {
                  <div className="bg-greyVivino flex flex-row pt-1 mb-1 h-screen">
                      <MenuDashboard />
                      <div className="overflow-y-auto w-full">
-                         <h1 className="font-plus-jakarta-sans pt-4 text-3xl text-center text-wine font-semibold">Favoritos</h1><br></br>
+                         <h1 className="font-plus-jakarta-sans pt-4 text-3xl text-center text-wine font-semibold">Caja del mes</h1><br></br>
                          <div className="flex flex-col items-center w-full">
                              <hr className="w-full border-gray-300" />
                              <br />
                              <div className="flex ml-36  items-center w-full">
                                  <LabelImportantIcon style={{ color: '#c23a2e' }} className="flex-shrink-0" />
-                                 <h2 className="font-plus-jakarta-sans text-2xl text-start font-semibold ml-2">Mis favoritos</h2>
+                                 <h2 className="font-plus-jakarta-sans text-2xl text-start font-semibold ml-2">Mis Cajas</h2>
                              </div>
                              <br />
-                             <MapUserFavoriteProducts />
+                             <SubscriptionBox />
                          </div>
                      </div>
                  </div>
@@ -54,4 +54,4 @@ const FavoriteProduct: React.FC = (): React.ReactNode => {
   )
 };
 
-export default FavoriteProduct;
+export default CajaMes;
--- liquors/src/app/profile/dashboardUser/historialReviews/page.tsx
+++ liquors/src/app/profile/dashboardUser/historialReviews/page.tsx
@@ -27,38 +27,30 @@ const HistorialReviews: React.FC = (): React.ReactNode => {
         } 
     }, [token])*/
 
-  return (
-    <>
-      {token && (
-        <>
-          <div className="bg-greyVivino flex flex-row pt-1 mb-1 h-auto">
-            <MenuDashboard />
-            <div className=" w-auto">
-              <h1 className="font-plus-jakarta-sans pt-4 text-3xl text-center text-wine font-semibold">
-                Reviews
-              </h1>
-              <br></br>
-              <div className="flex flex-col items-center w-full">
-                <hr className="w-full border-gray-300" />
-                <br />
-                <div className="flex ml-36 items-center w-full">
-                  <LabelImportantIcon
-                    style={{ color: "#c23a2e" }}
-                    className="flex-shrink-0"
-                  />
-                  <h2 className="font-plus-jakarta-sans text-2xl text-start font-semibold ml-2">
-                    Mis reviews
-                  </h2>
+    return (
+      <>
+          {token && (
+              <>
+                <div className="bg-greyVivino flex flex-row pt-1 mb-1 h-screen">
+                    <MenuDashboard/>
+                    <div className="overflow-y-auto w-full">
+                        <h1 className="font-plus-jakarta-sans pt-4 text-3xl text-center text-wine font-semibold">Reviews</h1><br></br>
+                        <div className="flex flex-col items-center w-full">
+                            <hr className="w-full border-gray-300" />
+                            <br />
+                            <div className="flex ml-36 items-center w-full">
+                                <LabelImportantIcon style={{ color: '#c23a2e' }} className="flex-shrink-0" />
+                                <h2 className="font-plus-jakarta-sans text-2xl text-start font-semibold ml-2">Mis reviews</h2>
+                            </div>
+                            <br />
+                            <MapUserHistorialReviews />
+                        </div>
+                    </div>
                 </div>
-                <br />
-                <MapUserHistorialReviews />
-              </div>
-            </div>
-          </div>
-        </>
-      )}
-    </>
-  );
+              </>
+          )}
+      </>
+)
 };
 
 export default HistorialReviews;
