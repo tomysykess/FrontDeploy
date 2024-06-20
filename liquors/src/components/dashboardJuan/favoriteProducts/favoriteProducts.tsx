9ec961920cf0d6e9491598cfa6e22b08577fefc3
tomas.avila <tomysykess@gmail.com>
2024-06-20 14:07:02 -0300

    color

--- liquors/src/components/dashboardJuan/favoriteProducts/favoriteProducts.tsx
+++ liquors/src/components/dashboardJuan/favoriteProducts/favoriteProducts.tsx
@@ -21,7 +21,6 @@ export const FavoriteProductsCard = ({
     if (idUser) {
       setIdUser(idUserParsed.id);
     }
-
   }, []);
 
   //handler borrado favs
@@ -30,9 +29,8 @@ export const FavoriteProductsCard = ({
     deleteFavorites(userId, productId, dispatch);
   };
 
-
   return (
-    <div className="flex items-start max-w-2xl pr-72 border-r-8 font-plus-jakarta-sans border-r-wine shadow-lg bg-white dark:bg-darkMode-greyVivino rounded-lg p-4 m-4 relative">
+    <div className="flex items-start max-w-2xl pr-72 border-r-8 font-plus-jakarta-sans border-r-wine shadow-lg  dark:bg-darkMode-greyVivino rounded-lg p-4 m-4 relative">
       {/* Imagen a la izquierda */}
       <div className="flex-shrink-0 h-full ">
         <img
